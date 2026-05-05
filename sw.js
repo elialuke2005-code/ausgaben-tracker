16e6a2cf0d52dc9e6c5fba8352195327f46c8bfb
elialuke2005-code <elialuke2005@github.com>
2026-05-05 17:12:27 +0200

    Fix: Zoom, Bounce und Browser-Gesten deaktiviert (PWA-Feel)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausgaben-v11';
+const CACHE = 'ausgaben-v12';
 const FILES = ['./', './index.html', './css/style.css', './js/app.js', './js/firebase-config.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
